02309a5acc3a151d9759e7ab4a62301b88162b80
Verma79 <bhavnaverma1779@gmail.com>
2021-09-15 13:42:43 +0530

    This is my first commit

---
+++ LtiAirlines/src/main/java/com/lti/client/Main.java
@@ -0,0 +1,34 @@
+package com.lti.client;
+
+import com.lti.model.Flight;
+
+public class Main {
+
+	public static void main(String[] args) {
+//		Flight f=new Flight(1001, "Mumbai", "Pune");
+//		System.out.println(f.getFlightId()+" "+f.getSource()+" "+f.getDestination());
+
+		//Test data
+		int num1=10;int num2=20;
+		//add(num1,num2): returned output
+		// 30           : expected output
+		if(add(num1,num2)==30) {      
+			System.out.println("Test passed.");
+		}
+		else {
+			System.out.println("Test failed.");
+		}
+		
+	}
+
+	public static int add(int n1,int n2) {
+		return n1+n2;
+	}
+	public static boolean compare(int n1,int n2) {
+		if(n1==n2) {
+			return true;
+		}
+		return false;
+	}
+}
+
